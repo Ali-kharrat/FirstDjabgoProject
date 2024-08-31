bb04f70d5bfe2fee4add5998a5f7ed61256c3485
N0X111 <53081539+Ali-kharrat@users.noreply.github.com>
2024-08-31 08:04:49 +0330

    Add files via upload

---
+++ add_cart/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+# class AddCartForm(forms.Form):
+#     product_id = forms.HiddenInput()
+#     quantity = forms.IntegerField()
---
+++ add_cart/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+# from .views import adds_cart
+
+urlpatterns = [
+    # path('add/<int:product_id>', adds_cart, name='add')
+]
